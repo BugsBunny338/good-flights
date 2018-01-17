d40e693cf1049031a20c79215caf2b396e94c8f3
Jan Sovka <jan.sovka@gooddata.com>
2018-01-16 16:34:35 -0800

    New header visual, removing extra container

--- src/components/NavMainMenu/NavMainMenu.js
+++ src/components/NavMainMenu/NavMainMenu.js
@@ -31,24 +31,24 @@ class NavMainMenu extends Component {
 
     render() {
         return (
-            <Container fluid={true}>
                 <Row>
-                    <Col xs={12}>
-                        <Navbar className="navbar-dark bg-dark .navbar-expand-sm">
+                    <Col xs={12} className="nopadding">
+                        <Navbar className="navbar-dark bg-dark .navbar-expand-sm Navbar">
                             <NavbarBrand href="/">
-                                <h5 style={{color: 'white', verticalAlign: 'middle'}}>
-                                    <img src={logo} style={{width: '20px', height: '20px', marginRight: '15px'}}
-                                         className=".navbar-brand" alt="GoodData"/>GoodData Flight Center</h5>
+                                <h5 className="Logo">
+                                    <img className=".navbar-brand LogoImage" src={logo} alt="GoodData"/>
+                                    <span className="LogoThick">GoodData</span> <span className="LogoThin">Flight Center</span>
+                                </h5>
                             </NavbarBrand>
                             <Nav>
-                                <Col md={10}/>
-                                <NavItem>
+                                <Col xs={10}/>
+                                <NavItem className="NavItem">
                                     <NavLink href=""
                                              onClick={(e) => this.setPages(e,[{ page:<FlightSearchPage/>, breadcrumb:'Routes'}])}>
                                         Routes
                                     </NavLink>
                                 </NavItem>
-                                <NavItem>
+                                <NavItem className="NavItem">
                                     <NavLink href=""
                                              onClick={(e) => this.setPages(e, [{page: <ScheduledFlightSearchPage/>, breadcrumb:'Flights' }])}>
                                         Flights
@@ -58,7 +58,6 @@ class NavMainMenu extends Component {
                         </Navbar>
                     </Col>
                 </Row>
-            </Container>
 
         );
     }
