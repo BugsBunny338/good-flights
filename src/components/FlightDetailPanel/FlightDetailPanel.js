081251dd9bcc91e727c9b6368bf8173a40b0e8eb
zsvoboda <zsvoboda@gmail.com>
2018-01-18 15:50:18 -0800

    Flight Details

--- src/components/FlightDetailPanel/FlightDetailPanel.js
+++ src/components/FlightDetailPanel/FlightDetailPanel.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Row, Col} from 'reactstrap';
 import {connect} from "react-redux";
 
-import { Kpi } from '@gooddata/react-components';
+import {Kpi} from '@gooddata/react-components';
 
 import cfg from '../../config';
 import catalogJson from '../../catalog';
@@ -16,35 +16,33 @@ class FlightDetailPanel extends Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.afm = this.afm.bind(this);
 
     }
 
+    afm(carrier, flight) {
+        return {filter:[
+        {
+            id: C.attributeDisplayForm('Carrier'),
+            type: 'attribute',
+            in: [carrier]
+        },
+        {
+            id: C.attributeDisplayForm('Flight Number'),
+            type: 'attribute',
+            in: [flight]
+        }]};
+    };
+
     render() {
         const {origin, destination, flight, carrier} = this.props.data;
+        const _c = this;
         return (
             <div className="FlightDetailPanel">
                 <Row>
-                    <Col xs={3}>
-                        <h1 className="FlightDetailPanel-h1">
-                            <Kpi filters={[
-                                {
-                                    id: C.attributeDisplayForm('Carrier'),
-                                    type: 'attribute',
-                                    in: [carrier]
-                                },
-                                {
-                                    id: C.attributeDisplayForm('Flight Number'),
-                                    type: 'attribute',
-                                    in: [flight]
-                                }
-                            ]} measure={C.metric('On Time Flights (%)')} projectId={cfg.projectId}/>
-                        </h1>
-                        <h1>TEST</h1>
-                    </Col>
-                </Row>
-                <Row>
                     <Col xs={12}>
-                        <h1 className="FlightDetailPanel-h1">{origin.label} => {destination.label} Airlines Benchmark</h1>
+                        <h1 className="FlightDetailPanel-h1">{origin.label} to {destination.label} Airlines
+                            Benchmark</h1>
                     </Col>
                 </Row>
                 <Row>
